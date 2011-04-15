f2121f752d2de6506f6176725efd7cde58c5a898
David Vilar <david.vilar@dfki.de>
2011-04-15 17:09:35 +0200

    Changed hGetContents with getContents

--- invertAlignment.hs
+++ invertAlignment.hs
@@ -8,6 +8,6 @@ invertAlignment :: Alignment -> Alignment
 invertAlignment = map (\(x, y) -> (y, x))
 
 main = do
-    aFile <- hGetContents stdin
+    aFile <- getContents
     let a = parseAlignmentFile aFile
     putStrLn $ unlines $ map printAlignment (map invertAlignment a)
